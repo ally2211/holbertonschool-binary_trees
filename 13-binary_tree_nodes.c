bc3d23f8aef8cee93332a63170685317e75b5478
root <6677@holbertonstudents.com>
2023-08-17 17:43:52 -0700

    nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
+ * @tree: Pointer to the root node of the tree to count the number of nodes.
+ *
+ * Return: Number of nodes with at least one child, or 0 if tree is NULL.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/*check if is a leaf node */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	/* count for the current node has at least one child. */
+	size_t count = 1;
+
+	/* Recursively count the nodes in left subtree */
+	if (tree->left != NULL)
+		count += binary_tree_nodes(tree->left);
+
+	/* Recursively count the nodes in the right subtree */
+	if (tree->right != NULL)
+		count += binary_tree_nodes(tree->right);
+
+	/* Return the total count.*/
+	return (count);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	size_t left_height, right_height;
+
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	size_t left_height = 0;
-	
-	right_height = 0;
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
-
-	return (left_height > right_height ? left_height : right_height);
+	if (left_height > right_height)
+		return (1 + left_height);
+	else
+		return (1 + right_height);
 }
