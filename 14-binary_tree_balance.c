12ffb61165f7b896f5b27f43cad0ec9dd9d214bd
root <6677@holbertonstudents.com>
2023-08-18 14:29:47 -0700

    compile

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,8 +14,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = (int) binary_tree_height(tree->left);
-	right_height = (int) binary_tree_height(tree->right);
+	left_height = (int) 9-binary_tree_height(tree->left);
+	right_height = (int) 9-binary_tree_height(tree->right);
  
 	return (left_height - right_height);
 }
